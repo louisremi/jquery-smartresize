1e57d496092db4725f855dabf8da7b6c092e824c
Louis-Rémi Babé <public@lrbabe.com>
2011-04-13 05:19:17 -0700

    update link

--- jquery.smartresize.js
+++ jquery.smartresize.js
@@ -2,7 +2,7 @@
  * smartresize: debounced resize event for jQuery
  *
  * latest version and complete README available on Github:
- * https://github.com/lrbabe/jquery.smartresize.js
+ * https://github.com/louisremi/jquery.smartresize.js
  *
  * Copyright 2011 @louis_remi
  * Licensed under the MIT license.
